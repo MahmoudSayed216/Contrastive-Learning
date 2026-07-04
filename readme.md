061268284a38bce9d1a8c0bff3d06c6c207ac450
MahmoudSayed216 <mahmoud_das@outlook.com>
2026-07-04 23:21:47 +0300

    writing the report

---
+++ readme.md
@@ -0,0 +1,9 @@
+## Contrastive Learning using Triplet Loss and Contrastive Loss on the MNIST dataset.
+
+
+This is a Comprehensive notebook showcasing some of the differences between Contrastive Learning via Contrastive Loss and Triplet Loss
+
+
+
+
+
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 # from model import SiameseRIZZNet
+from TripletLoss.model import SiameseRIZZNet
 # import torch
 
 # model = SiameseRIZZNet()
@@ -23,21 +24,47 @@
 # print("Model size: ", size_in_mb)
 
 
-from dataset import MNISTDataset
+# from dataset import MNISTDataset
+# import matplotlib.pyplot as plt
+
+# data = MNISTDataset("./data/mnist_train.csv")
+
+# anc, pos, neg = data[0]
+
+# print(anc.shape)
+# print(pos.shape)
+# print(neg.shape)
+
+
+# # plt.imshow(anc.reshape(28, 28), cmap='gray')
+# # plt.show()
+# # plt.imshow(pos.reshape(28, 28), cmap='gray')
+# # plt.show()
+# # plt.imshow(neg.reshape(28, 28), cmap='gray')
+# # plt.show()
+
+from collections import defaultdict
+
 import matplotlib.pyplot as plt
-
-data = MNISTDataset("./data/mnist_train.csv")
-
-anc, pos, neg = data[0]
-
-print(anc.shape)
-print(pos.shape)
-print(neg.shape)
+import torch
+import visualtorch
+from torch import nn
 
 
-# plt.imshow(anc.reshape(28, 28), cmap='gray')
-# plt.show()
-# plt.imshow(pos.reshape(28, 28), cmap='gray')
-# plt.show()
-# plt.imshow(neg.reshape(28, 28), cmap='gray')
-# plt.show()
+model = SiameseRIZZNet(128, 0.01)
+model.eval()
+input_shape = input_shape = (1, 1, 28, 28)
+color_map: dict = defaultdict(dict)
+color_map[nn.Conv2d]["fill"] = "#E69F00"
+color_map[nn.BatchNorm2d]["fill"] = "#009E73"
+color_map[nn.ReLU]["fill"] = "#56B4E9"
+
+img_default = visualtorch.render(model, input_shape, style="flow", color_map=color_map, legend=True)
+
+
+dpi = 150  # rendered at 2x this in the final doc build (savefig.dpi=300 in conf.py)
+plt.figure(figsize=(img_default.width / dpi, img_default.height / dpi), dpi=dpi)
+plt.imshow(img_default)
+plt.axis("off")
+plt.tight_layout()
+plt.show()
